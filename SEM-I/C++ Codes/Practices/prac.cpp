10bb7c95ca8ca7ffb7703b545bfc768822b0de6a
Muhammad Tamjeed Hussain <166079389+mTamjeedhussain67@users.noreply.github.com>
2025-11-11 01:18:48 +0500

    Delete SEM-I/C++ Codes directory

--- SEM-I/C++ Codes/Practices/prac.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main () {
-    int x = 5;
-    int &refx = x;
-    cout << refx;
-}
--- SEM-I/C++ Codes/pp.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main () {
-    char x = 'F';
-    x += 'a' - 'A';
-    cout << x;
-}
